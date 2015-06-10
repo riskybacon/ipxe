398c4d43793cf2e665aa36dd9ad163a7ef126005
Jeff Bowles <jbowles@newmexicoconsortium.org>
2015-06-10 09:22:57 -0600

    Added emulab / nmc licence

--- src/emulab/bootinfo.c
+++ src/emulab/bootinfo.c
@@ -1,24 +1,25 @@
 /*
- * Copyright (C) 2013 Michael Brown <mbrown@fensystems.co.uk>.
+ * Copyright (c) 2000-2015 New Mexico Consortium
+ * 
+ * {{{EMULAB-LICENSE
+ * 
+ * This file is part of the Emulab network testbed software.
+ * 
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * }}}
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA.
- *
- * You can also choose to distribute this program under the terms of
- * the Unmodified Binary Distribution Licence (as given in the file
- * COPYING.UBDL), provided that you have satisfied its requirements.
  */
 
 FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
@@ -108,7 +109,6 @@ struct bootinfo_request {
 	/** Retry timer */
 	struct retry_timer timer;
 	struct boot_info bootinfo;
-
 };
 
 int bootinfo (const char * hostname) {
